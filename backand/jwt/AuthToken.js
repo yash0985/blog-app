97b011fd22cb63007146133ed4003bf6ca08f545
Amit <yga995@gmail.com>
2025-02-01 19:38:47 +0530

    Completed

--- backand/jwt/AuthToken.js
+++ backand/jwt/AuthToken.js
@@ -7,10 +7,10 @@ const createTokenAndSaveCookies = async (userId, res) => {
     expiresIn: "30d",
   });
   res.cookie("jwt", token, {
-    httpOnly: false, // Temporarily set to false for testing
-    secure: true,
-    sameSite: "noneg",
-    // path: "/", // Ensure the cookie is available throughout the site
+    httpOnly: true, // Temporarily set to false for testing
+    secure: false,
+    sameSite: "lax",
+    path: "/", // Ensure the cookie is available throughout the site
   });
   await User.findByIdAndUpdate(userId, { token });
   return token;
