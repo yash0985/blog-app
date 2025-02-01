78366b2500f5cb53c63367210d72542cf5125f2b
Amit <yga995@gmail.com>
2025-02-01 18:28:49 +0530

    Updated url

--- backand/jwt/AuthToken.js
+++ backand/jwt/AuthToken.js
@@ -1,21 +1,3 @@
-// import jwt from "jsonwebtoken"
-// import { User } from "../models/user.model.js"
-
-// const creatTokenAndSaveCookies  = async(userId,res)=>{
-//     const token = jwt.sign({userId},process.env.JWT_SECRET_KEY,{
-//         expiresIn: "7d"
-//     })
-//     res.cookie("jwt",token,{
-//         httpOnly:true,//xss
-//         secure: true,
-//         sameSite:"strict"//csrf
-//     })
-//     await User.findByIdAndUpdate(userId,{token})
-//     return token;
-// }
-
-// export default creatTokenAndSaveCookies;
-
 
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
@@ -25,9 +7,9 @@ const createTokenAndSaveCookies = async (userId, res) => {
     expiresIn: "30d",
   });
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
-    secure: false,
-    sameSite: "lax",
+    httpOnly: false, // Temporarily set to false for testing
+    secure: true,
+    sameSite: "none",
     path: "/", // Ensure the cookie is available throughout the site
   });
   await User.findByIdAndUpdate(userId, { token });
